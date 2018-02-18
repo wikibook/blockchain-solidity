fb8493641d7715fc4c8c2f91ca591134038c3b24
CHO <rygh4775@gmail.com>
2018-02-19 00:16:22 +0900

    메소드 선언문 에러 코드 수정(public 위치 조정) 에러 내용 하단 ----------------------------------------- Error: Expected token LBrace got 'Public'     function getTransaction(bytes32 user_id, bytes32 project_id) constant returns (string tran_data) public {                                                                                                      ^

--- chap5/ch5_02_TransactionLogNG.sol
+++ chap5/ch5_02_TransactionLogNG.sol
@@ -6,13 +6,13 @@ contract TransactionLogNG {
     mapping (bytes32 => mapping (bytes32 => string)) public tranlog;
 
     // (3) 거래 내용 등록
-    function setTransaction(bytes32 user_id, bytes32 project_id, string tran_data) public{
+    function setTransaction(bytes32 user_id, bytes32 project_id, string tran_data) public {
         // (4) 등록
         tranlog[user_id][project_id] = tran_data;
     }
     
     // (5) 사용자, 프로젝트별 거래 내용을 가져온다
-    function getTransaction(bytes32 user_id, bytes32 project_id) constant returns (string tran_data) public {
+    function getTransaction(bytes32 user_id, bytes32 project_id) public constant returns (string tran_data) {
         return tranlog[user_id][project_id];
     }
 }
